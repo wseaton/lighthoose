9d5b26d833c919144ad3fca8280da27b4531e3b1
Will Eaton <wseaton93@gmail.com>
2019-06-26 11:54:20 -0400

    disable scan for external trigger

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('log-timestamp');
 
 const cron = require("node-cron");
-const scan = require("./scan.js");
+// const scan = require("./scan.js");
 
-cron.schedule('0 1 * * *', scan, { timezone: "America/New_York" });
+// cron.schedule('0 1 * * *', scan, { timezone: "America/New_York" });
